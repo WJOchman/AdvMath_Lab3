aea33da6d61461aa31f6bf6c1a8bfb5d954d13c1
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2021-11-22 22:11:10 +0000

    Update Lab3.m

--- Lab3.m
+++ Lab3.m
@@ -17,41 +17,38 @@ clc % clears the screen
 
 %Task 2: Circular convolution
 
-x_n = [3 7 2 6 1 3 9];
-h_n = [2 3 -2 5 3];
+x_n = [3 7 2 6 1 3 9];  % input sequence
+h_n = [2 3 -2 5 3];     % impulse response 
 
 disp("Linear: ");
 
-tic   %take current processor time
-
-y_n = conv(x_n,h_n);
-
-toc   %take current processor time
+tic                     % take current processor time
+y_n = conv(x_n,h_n);    % calculate convolution sum using linear 
+toc                     %take current processor time
 
 disp("Circular: ");
-tic
 
-y_cn = cconv(x_n,h_n);    %Gets circular convolution
-
-toc
+tic                     % take current processor time
+y_cn = cconv(x_n,h_n);  % calculate convolution sum using circular
+toc                     % take current processor time
 
 %newline -> creates 1 newline
 disp(newline + "With random array:- " + newline)
 
-x_nr=randn(100000,1); % array that contains 100k random numbers
-h_nr=randn(100000,1); % array that contains 100k random numbers
+x_nr=randn(100000,1);   % array that contains 100k random numbers
+h_nr=randn(100000,1);   % array that contains 100k random numbers
 
 disp("Linear: ");
 
-tic % take current processor time
-y_nr=conv(x_nr,h_nr); % calculate convolution sum using linear
-toc % take current processor time
+tic                     % take current processor time
+y_nr=conv(x_nr,h_nr);   % calculate convolution sum using linear
+toc                     % take current processor time
 
 disp("Circular: ");
 
-tic % take current processor time
+tic                     % take current processor time
 y_cnr=cconv(x_nr,h_nr); % calculate convolution sum using circular
-toc % take current processor time
+toc                     % take current processor time
 
 
 %While using small array, the linear convolution seems to faster but
